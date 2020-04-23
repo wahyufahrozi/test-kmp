6e86f152ac5f7002b25a9a6b413320766312b316
wahyufahrozi <wahyufahrozi97@gmail.com>
2020-04-23 09:31:14 +0700

    update color

--- src/components/gallery.js
+++ src/components/gallery.js
@@ -1,5 +1,12 @@
 import React from "react";
 import "./gallery.css";
 export default function Gallery(props) {
-  return <div className="square">{props.data.attributes.name}</div>;
+  return (
+    <div
+      className="square"
+      style={{ backgroundColor: props.data.attributes.color }}
+    >
+      {props.data.attributes.name}
+    </div>
+  );
 }
--- src/helpers/data-dummy.js
+++ src/helpers/data-dummy.js
@@ -2,201 +2,241 @@ const DataDummy = [
   {
     attributes: {
       name: "col",
+      color: "red,",
     },
   },
   {
     attributes: {
       name: "col",
+      color: "green",
     },
   },
   {
     attributes: {
       name: "col",
+      color: "blue",
     },
   },
   {
     attributes: {
       name: "col",
+      color: "yellow",
     },
   },
   {
     attributes: {
       name: "col",
+      color: "blue",
     },
   },
   {
     attributes: {
       name: "col",
+      color: "brown",
     },
   },
   {
     attributes: {
       name: "col",
+      color: "grey",
     },
   },
   {
     attributes: {
       name: "col",
+      color: "purple",
     },
   },
   {
     attributes: {
       name: "col",
+      color: "grey",
     },
   },
   {
     attributes: {
       name: "col",
+      color: "purple",
     },
   },
   {
     attributes: {
       name: "col",
+      color: "pink",
     },
   },
   {
     attributes: {
       name: "col",
+      color: "red,",
     },
   },
   {
     attributes: {
       name: "col",
+      color: "green",
     },
   },
   {
     attributes: {
       name: "col",
+      color: "blue",
     },
   },
   {
     attributes: {
       name: "col",
+      color: "yellow",
     },
   },
   {
     attributes: {
       name: "col",
+      color: "blue",
     },
   },
   {
     attributes: {
       name: "col",
+      color: "brown",
     },
   },
   {
     attributes: {
       name: "col",
+      color: "grey",
     },
   },
   {
     attributes: {
       name: "col",
+      color: "purple",
     },
   },
   {
     attributes: {
       name: "col",
+      color: "pink",
     },
   },
   {
     attributes: {
       name: "col",
+      color: "red,",
     },
   },
   {
     attributes: {
       name: "col",
+      color: "green",
     },
   },
   {
     attributes: {
       name: "col",
+      color: "blue",
     },
   },
   {
     attributes: {
       name: "col",
+      color: "yellow",
     },
   },
   {
     attributes: {
       name: "col",
+      color: "blue",
     },
   },
   {
     attributes: {
       name: "col",
+      color: "red,",
     },
   },
   {
     attributes: {
       name: "col",
+      color: "green",
     },
   },
   {
     attributes: {
       name: "col",
+      color: "blue",
     },
   },
   {
     attributes: {
       name: "col",
+      color: "purple",
     },
   },
   {
     attributes: {
       name: "col",
+      color: "pink",
     },
   },
   {
     attributes: {
       name: "col",
+      color: "yellow",
     },
   },
   {
     attributes: {
       name: "col",
+      color: "blue",
     },
   },
   {
     attributes: {
       name: "col",
+      color: "grey",
     },
   },
   {
     attributes: {
       name: "col",
+      color: "purple",
     },
   },
   {
     attributes: {
       name: "col",
+      color: "red,",
     },
   },
   {
     attributes: {
       name: "col",
+      color: "yellow",
     },
   },
   {
     attributes: {
       name: "col",
+      color: "blue",
     },
   },
   {
     attributes: {
       name: "col",
+      color: "green",
     },
   },
   {
     attributes: {
       name: "col",
+      color: "blue",
     },
   },
   {
     attributes: {
       name: "col",
+      color: "pink",
     },
   },
 ];
